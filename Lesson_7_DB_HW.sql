ce27a9a3e65797f206e8c8102a71aa86eb39aa4c
psmilovanov <psmilovanov@gmail.com>
2020-06-15 15:51:36 +0300

    Add 3rd task to h/w

--- Lesson_7_DB_HW.sql
+++ Lesson_7_DB_HW.sql
@@ -17,3 +17,38 @@ select * from users where id in (select user_id from orders);
 /*Задача 2*/
 
 select name, price, (select name from catalogs where id = catalog_id) as catalogs from products;
+
+/* Задача 3*/
+
+insert into cities 
+	(label, name)
+values 
+	('moscow', 'Москва'),
+	('irkutsk', 'Иркутск'),
+	('omsk', 'Омск'),
+	('kazan', 'Казань'),
+	('novgorod', 'Новгород');
+
+
+
+drop table if exists flights;
+create table flights (
+	id serial primary key, 
+	`from` varchar(255),
+	`to` varchar(255),
+	foreign key (`from`) references cities(label),
+	foreign key (`to`) references cities(label)
+	); 
+
+insert into flights 
+	(`from`, `to`)
+values
+	('moscow', 'omsk'),
+	('novgorod', 'kazan'),
+	('irkutsk', 'moscow'),
+	('omsk', 'irkutsk'),
+	('moscow', 'kazan');
+	
+
+
+select id, (select name from cities where label = `from`) as 'Откуда', (select name from cities where label = `to`) as 'Куда'  from flights;
